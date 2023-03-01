0d7cb48d678a489db32323f13003ad66fc397369
ellinarel <113920230+ellinarel@users.noreply.github.com>
2023-03-01 23:58:00 +0200

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+import Notiflix from 'notiflix';
+import 'flatpickr/dist/flatpickr.min.css';
+const form = document.querySelector('.form');
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+const delay = Number(form.delay.value);
+const step = Number(form.step.value);
+const amount = Number(form.amount.value);
+  
+  for (let i = 0; i <= amount; i += 1) {
+    const position = i;
+    const Promdelay = delay + (i - 1) * step;
+    createPromise(position, Promdelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+  
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`
+        );
+      })
+  }
+})
+
+
+
